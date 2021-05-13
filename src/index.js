9eefe52fd5f39ec1ea1b29548dbc1c83341b53b1
Hugo Naaman Castro <hugonaaman@gmail.com>
2021-05-13 20:44:34 -0300

    Tests suceeded

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = user;
+
+  next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if (user.todos <= 10 || user.pro) {
+    next();
+  } else {
+    return response.status(403).json({ error: "Limit exceeded for free" })
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const validUuid = validate(id);
+
+  if (!validUuid) {
+    return response.status(400).json({
+      error: "Id must be a valid uuid"
+    })
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: "User not found"
+    })
+  }
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: "Todo not found"
+    })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
+
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({
+      error: "User not found by id"
+    });
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -67,7 +121,6 @@ app.patch('/users/:id/pro', findUserById, (request, response) => {
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
-
   return response.json(user.todos);
 });
 
